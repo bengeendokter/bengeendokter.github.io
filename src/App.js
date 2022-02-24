df64848cc6bb1a26e814a4596019f15a2a659fb6
Ben <45427540+bengeendokter@users.noreply.github.com>
2022-02-24 17:52:44 +0100

    message Yoran

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App()
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Nee Yoran, het is nog niet af.
         </p>
         <a
           className="App-link"
