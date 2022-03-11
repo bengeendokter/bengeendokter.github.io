97a707ddadf0d55ad474e97f4f63e17dca624f22
Ben <45427540+bengeendokter@users.noreply.github.com>
2022-03-11 13:21:29 +0100

    eerste html

--- src/App.js
+++ src/App.js
@@ -6,12 +6,38 @@ function App()
 {
   return (
     < >
-        <p>
-          Nee Yoran, het is nog niet af.
-        </p>
-        <ExtLink href="https://reactjs.org">
-          test
-        </ExtLink>
+      <header>
+        <a href="#main-content">Navigatie overslaan</a>
+        <nav>
+          <ul>
+            <li>Projecten</li>
+            <li>CV</li>
+          </ul>
+        </nav>
+      </header>
+      <main id='main-content'>
+        <section title='intro'>
+          <img src="" alt="foto van mezelf" />
+          <p>Ben Arts</p>
+          <p>geen dokter</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, et? Nesciunt nihil, magni labore quidem ullam delectus molestiae eius rem dicta ipsa adipisci perferendis beatae quae sunt saepe quaerat laborum?</p>
+        </section>
+        <section aria-labelledby='projecten-title'>
+          <h2 id='projecten-title'>Projecten</h2>
+
+        </section>
+        <section aria-labelledby='CV-title'>
+          <h2 id='CV-title'>CV</h2>
+
+          <button>Download CV</button>
+        </section>
+      </main>
+      <footer>
+        <aside>
+          <ExtLink href='https://github.com/bengeendokter' >github</ExtLink>
+        </aside>
+      </footer>
+
     </>
   );
 }
