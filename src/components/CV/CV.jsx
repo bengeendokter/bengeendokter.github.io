1bbbefad458acf85a5b7b391653aa9aa106f3601
Ben <45427540+bengeendokter@users.noreply.github.com>
2022-04-03 22:14:26 +0200

    cv aanvullingen

--- src/components/CV/CV.jsx
+++ src/components/CV/CV.jsx
@@ -6,33 +6,33 @@ export default function CV({children, ...rest})
     return (
         <article className={CVCSS.main}>
             <section title='naam'>
-                <p>Ben Arts</p>
+                <h3>Ben Arts</h3>
             </section>
             <section aria-labelledby='profiel-title'>
-                <h3 id='profiel-title'>profiel</h3>
+                <h4 id='profiel-title'>profiel</h4>
                 <p>tags/kernwoorden</p>
 
                 <p>url LinkedIn-profiel</p>
-                <p>+32 474 53 04 30</p>
+                <p>+32 400 00 00 00</p>
                 <p>ben.arts.mail@gmail.com</p>
                 <p>27/03/2000</p>
-                <p>Ledebaan 168, 9300 Aalst</p>
+                <p>Straatnaam 0, 0000 Stad</p>
             </section>
             <section aria-labelledby='vaardigheden-title'>
-                <h3 id='vaardigheden-title'>vaardigheden</h3>
+                <h4 id='vaardigheden-title'>vaardigheden</h4>
                 <p>programeertalen</p>
                 <p>talen</p>
             </section>
             <section aria-labelledby='werkervaring-title'>
-                <h3 id='werkervaring-title'>werkervaring</h3>
+                <h4 id='werkervaring-title'>werkervaring</h4>
                 <p>aviapartner</p>
             </section>
             <section aria-labelledby='opleiding-title'>
-                <h3 id='opleiding-title'>opleiding</h3>
+                <h4 id='opleiding-title'>opleiding</h4>
                 <p>hogent toeg. inf.</p>
             </section>
             <section aria-labelledby='hobbies-title'>
-                <h3 id='hobbies-title'>hobbies</h3>
+                <h4 id='hobbies-title'>hobbies</h4>
             </section>
         </article>
     );
