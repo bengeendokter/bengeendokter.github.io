937acb8493768dfccb4e5d05e9b6d517a7d697e4
Ben <45427540+bengeendokter@users.noreply.github.com>
2022-04-03 20:32:42 +0200

    cv html tags structuur

--- src/components/CV/CV.jsx
+++ src/components/CV/CV.jsx
@@ -5,28 +5,35 @@ export default function CV({children, ...rest})
 {
     return (
         <article className={CVCSS.main}>
-            
-            
-            Arts
-            Ben
+            <section title='naam'>
+                <p>Ben Arts</p>
+            </section>
+            <section aria-labelledby='profiel-title'>
+                <h3 id='profiel-title'>profiel</h3>
+                <p>tags/kernwoorden</p>
 
-            tags/kernwoorden
-
-            url LinkedIn-profiel 
-            
-            +32 474 53 04 30
-            ben.arts.mail@gmail.com
-            27/03/2000
-            Ledebaan 168, 9300 Aalst
-
-            vaardigheden/programeertalen
-            talen
-
-            werkervaring
-
-            opleiding
-
-            hobbies/nevenactiviteiten/hobbies
+                <p>url LinkedIn-profiel</p>
+                <p>+32 474 53 04 30</p>
+                <p>ben.arts.mail@gmail.com</p>
+                <p>27/03/2000</p>
+                <p>Ledebaan 168, 9300 Aalst</p>
+            </section>
+            <section aria-labelledby='vaardigheden-title'>
+                <h3 id='vaardigheden-title'>vaardigheden</h3>
+                <p>programeertalen</p>
+                <p>talen</p>
+            </section>
+            <section aria-labelledby='werkervaring-title'>
+                <h3 id='werkervaring-title'>werkervaring</h3>
+                <p>aviapartner</p>
+            </section>
+            <section aria-labelledby='opleiding-title'>
+                <h3 id='opleiding-title'>opleiding</h3>
+                <p>hogent toeg. inf.</p>
+            </section>
+            <section aria-labelledby='hobbies-title'>
+                <h3 id='hobbies-title'>hobbies</h3>
+            </section>
         </article>
     );
 };
