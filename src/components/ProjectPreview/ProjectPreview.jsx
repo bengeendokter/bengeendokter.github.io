8350900e9cca0127998ca77ccbc9f249fa878acd
Ben <45427540+bengeendokter@users.noreply.github.com>
2022-04-10 23:33:16 +0200

    github links per project

--- src/components/ProjectPreview/ProjectPreview.jsx
+++ src/components/ProjectPreview/ProjectPreview.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import Tag from '../Tag';
 import ProjectPreviewCSS from './ProjectPreview.module.css';
+import { ReactComponent as GithubSVG } from '../SVGIcons/github.svg';
+import ExtLink from '../ExtLink';
 
-export default function ProjectPreview({title, tags, beschrijving, imgSrc, imgAlt, ...rest})
+export default function ProjectPreview({title, tags, beschrijving, imgSrc, imgAlt, github, ...rest})
 {
     return (
         <div className={ProjectPreviewCSS["project-preview"]}>
             <img src={imgSrc} alt={imgAlt} className={ProjectPreviewCSS["project-image"]} />
             <p className={ProjectPreviewCSS["project-title"]}>{title}</p>
-            <div className={ProjectPreviewCSS["tags"]}>{tags.map((tag, i) => <Tag key={i}>{tag}</Tag>)}</div>
+            <div className={ProjectPreviewCSS["tags"]}>
+                {github && <ExtLink href={github} ><GithubSVG className='svg-icon' /></ExtLink>}
+          {tags.map((tag, i) => <Tag key={i}>{tag}</Tag>)}</div>
             <p className={ProjectPreviewCSS["project-beschrijving"]}>{beschrijving}</p>
         </div>
     );
